b04c6729910f9c2e41a83be91de56c61b5a48bb5
SarahAvery <sarahashavery@gmail.com>
2021-01-26 16:41:11 -0500

    banner complete

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -194,7 +194,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/index.js":[function(require,module,exports) {
+},{"./..\\img\\pink-living-room-chair.jpg":[["pink-living-room-chair.1160f5f5.jpg","resources/img/pink-living-room-chair.jpg"],"resources/img/pink-living-room-chair.jpg"],"_css_loader":"../../../../AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/index.js":[function(require,module,exports) {
 "use strict";
 
 require("../resources/css/css-reset.css");
@@ -230,7 +230,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59501" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61813" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

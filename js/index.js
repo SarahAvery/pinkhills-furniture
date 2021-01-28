2285d4f7caa58f1ea6cf8a1a5725e69d9a1accb7
SarahAvery <sarahashavery@gmail.com>
2021-01-28 17:49:39 -0500

    before adding PHP

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
+//Reset
 import "../resources/css/css-reset.css";
+
+//Global
 import "../resources/sass/main";
 
+//Header
 import "../resources/sass/header";
+
+//Home Page
 import "../resources/sass/home-page";
 
+//Footer
 import "../resources/sass/footer";
 
 console.log("Hello");
